e3fc47e072533318dd442a063c6ef906aec7467b
hemc <hemanthx9@gmail.com>
2022-04-26 22:23:01 +0530

    Integrated with makeshift database

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -4,18 +4,6 @@ import Axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-grid-system";
 
-const GrowthMap : Record<number, String> = {
-    1: "Hello",
-    2: "Bello"
-}
-
-/*const GetFollowupDetailsSuccess = (response:AxiosResponse) => {
-    return response.data;
-    //if(response.data.nullObj==false){
-    //  "setLstFollowups(response.data.lstFollowups);
-    //}
-};*/
-
 
 const PostPage: React.FC = () => {
 
@@ -26,29 +14,40 @@ const PostPage: React.FC = () => {
     const [upvotes, setUpvotes] = useState<number>(1);
     const [downvotes, setDownvotes] = useState<number>(1);
     const [child, setChild] = useState<number>(1);
-    const [postId, setPostId] = useState<number>(2);
+    const [postId, setPostId] = useState<number>(1);
+    const [value, setValue] = useState<string>("");
 
     const [disabled, setDisabled] = useState<boolean>(false);
 
     const GetPostDetailsSuccess = (response:AxiosResponse) => {
-        setpId(response.data.postId);
-        setBody(response.data.body);
-        setUpvotes(response.data.upvotes);
-        setDownvotes(response.data.downvotes);
-        setChild(response.data.child);
+        if(!response.data.isNull){
+            
+            setpId(response.data.postId);
+            setBody(response.data.body);
+            setUpvotes(response.data.upvotes);
+            setDownvotes(response.data.downvotes);
+            setChild(response.data.child);
+            if(response.data.body != ""){
+                setDisabled(true);
+            }
+            console.log("Post Details " + response.data.postId + " " + response.data.body + " " + response.data.upvotes+ " " + response.data.downvotes + " " + response.data.child);
+        }
+        
     }
     const GetPostDetails = async() => {
+        setValue("");
         setBody("");
         setDisabled(false);
+
         const result = await Axios.get("http://localhost:3100/postProfile/" + postId.toString())
                     .then((response) => {console.log("fetched"); return GetPostDetailsSuccess(response)})
                     //.else((err: any) => {console.log("error"); return "error"});
+        setValue(body.toString());
+        console.log("Hello value " + value + " body " + body);
     }
 
     const navigateChild = () =>{
         setPostId(child);
-        //GetPostDetails();
-        console.log(postId);
     }
 
     const upvotePost = () => { 
@@ -66,17 +65,23 @@ const PostPage: React.FC = () => {
         setDisabled(true);
     }
 
+    const saveText = (text:React.ChangeEvent<HTMLTextAreaElement>) => {
+        console.log("Text getting saved")
+        setBody(text.target.value);
+        setValue(text.target.value);
+    }
+
     //<textarea onChange={(text) => setBody(text.target.value)} />
 
     useEffect(() => {
         GetPostDetails();
-    }, [postId]);
+    }, [postId]); //[]
 
     return(
         <Container>
             <Row> Post Details for Post {pId}</Row>
             <Row> 
-                <Col> Body </Col> <Col> <textarea disabled = {disabled} onChange={(text) => setBody(text.target.value)} /> </Col>
+                <Col> Body </Col> <Col> <textarea value = {body.toString()} disabled = {disabled} onChange={(text) => saveText(text)} /> </Col>
             </Row>
             <Row>
                 <button color="#841584" disabled = {disabled} onClick = {upvotePost}> Upvote {upvotes}</button>
@@ -88,7 +93,6 @@ const PostPage: React.FC = () => {
             <Row>
                 <Col><button color = "danger" disabled = {disabled} onClick={savePostBody}> Submit Post Text</button></Col>
             </Row>
-            <Row /> <Row />
 
             <Row>
                 <button color = "danger" onClick={navigateChild}>Go to Post {child} </button>
